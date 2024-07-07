5b8f5a3831b4e3b1e9b039f1f713b16778c46dc3
grif <b_evgeny@ukr.net>
2024-07-07 20:06:43 +0300

    Add new exports in Observables index and update package.json
    The Observables index has been updated with new exports: Observable, OrderedObservable, Collector, and functions from FunctionLibs. Additionally, the version of the package.json file was incremented, and the "main" property was updated to point to "src/outLib/index.js".

---
+++ src/Libraries/Observables/index.ts
@@ -0,0 +1,5 @@
+export {Observable} from './Observable'
+export {OrderedObservable} from './OrderedObservable'
+export {Collector} from './Collector'
+export {deleteFromArray} from './FunctionLibs'
+export {quickDeleteFromArray} from './FunctionLibs'
