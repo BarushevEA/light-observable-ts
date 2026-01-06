86f4dce8277f9ee3aec1b132dbc21b51844c2363
grif <b_evgeny@ukr.net>
2026-01-06 22:05:00 +0200

    Bump version to 2.15.3 in `package.json`.

---
+++ src/outLib/Collector.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Collector = void 0;
+const FunctionLibs_1 = require("./FunctionLibs");
+class Collector {
+    arr = [];
+    killed = false;
+    collect(...subscriptionLikeList) {
+        if (!this.killed)
+            this.arr.push(...subscriptionLikeList);
+    }
+    unsubscribe(subscriptionLike) {
+        if (this.killed)
+            return;
+        subscriptionLike?.unsubscribe();
+        (0, FunctionLibs_1.quickDeleteFromArray)(this.arr, subscriptionLike);
+    }
+    unsubscribeAll() {
+        if (this.killed)
+            return;
+        const arr = this.arr;
+        for (let i = 0; i < arr.length; i++)
+            arr[i].unsubscribe();
+        arr.length = 0;
+    }
+    size() {
+        if (this.killed)
+            return 0;
+        return this.arr.length;
+    }
+    destroy() {
+        this.unsubscribeAll();
+        this.arr.length = 0;
+        this.arr = 0;
+        this.killed = true;
+    }
+    get isDestroyed() {
+        return this.killed;
+    }
+}
+exports.Collector = Collector;
---
+++ src/outLib/OrderedObservable.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OrderedObservable = void 0;
+const Observable_1 = require("./Observable");
+const FunctionLibs_1 = require("./FunctionLibs");
+const OrderedSubscribeObject_1 = require("./OrderedSubscribeObject");
+class OrderedObservable extends Observable_1.Observable {
+    sortDirection = FunctionLibs_1.sortAscending;
+    setAscendingSort() {
+        this.sortDirection = FunctionLibs_1.sortAscending;
+        return this.sortByOrder();
+    }
+    setDescendingSort() {
+        this.sortDirection = FunctionLibs_1.sortDescending;
+        return this.sortByOrder();
+    }
+    sortByOrder() {
+        if (this.killed)
+            return false;
+        this.subs.sort(this.sortDirection);
+        return true;
+    }
+    subscribe(listener, errorHandler) {
+        if (!this.isListener(listener))
+            return undefined;
+        const subscribeObject = new OrderedSubscribeObject_1.OrderedSubscribeObject(this, false);
+        this.addObserver(subscribeObject, listener, errorHandler);
+        return subscribeObject;
+    }
+    pipe() {
+        if (this.killed)
+            return undefined;
+        const subscribeObject = new OrderedSubscribeObject_1.OrderedSubscribeObject(this, true);
+        this.subs.push(subscribeObject);
+        return subscribeObject;
+    }
+    unSubscribe(listener) {
+        if (this.killed)
+            return;
+        if (this.process && listener) {
+            this.trash.push(listener);
+            return;
+        }
+        this.subs && !(0, FunctionLibs_1.quickDeleteFromArray)(this.subs, listener);
+    }
+}
+exports.OrderedObservable = OrderedObservable;
